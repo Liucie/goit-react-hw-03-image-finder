bb6bc6f4db06abcb6622ff496922959ca49029e9
Liudmyla Kozachenko <viergee@gmail.com>
2021-10-21 23:48:46 +0300

    fix scroll

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -6,13 +6,6 @@ import Button from '../Button/Button';
 import Modal from '../Modal/Modal';
 import Loader from 'react-loader-spinner';
 
-const Status = {
-  IDLE: 'idle',
-  PENDING: 'pending',
-  RESOLVED: 'resolved',
-  REJECTED: 'rejected',
-};
-
 const base_url = 'https://pixabay.com/api/';
 const api_key = '23237261-94e774dc3474a501c481a5592';
 
@@ -21,49 +14,45 @@ const newPixabayFetch = new PixabayFetch(base_url, api_key);
 export class ImageGallery extends Component {
   state = {
     searchResults: [],
-    status: Status.IDLE,
+    isLoading: false,
     showModal: false,
     currentImg: '',
   };
 
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.searchValue !== this.props.searchValue) {
-      this.setState({ status: Status.PENDING });
+      this.setState({ isLoading: true });
       newPixabayFetch.resetPage();
       newPixabayFetch.searchQuery = this.props.searchValue;
       newPixabayFetch
         .searchPhotos()
         .then(searchResults => {
-          this.setState({ searchResults, status: Status.RESOLVED });
+          this.setState({ searchResults, isLoading: false });
         })
-        .catch(err => this.setState({ status: Status.REJECTED }))
-        .finally(() => {
-          this.setState({ status: Status.RESOLVED });
-          window.scrollTo({
-            top: document.documentElement.scrollHeight,
-            behavior: 'smooth',
-          });
+        .catch(err => {
+          alert('Error! Try again!');
+          console.log(err);
         });
     }
   }
   handleClick = () => {
-    this.setState({ status: Status.PENDING });
+    this.setState({ isLoading: true });
     newPixabayFetch.page = 1;
     newPixabayFetch
       .searchPhotos()
       .then(searchResults => {
         this.setState(prevState => ({
+          isLoading: false,
           searchResults: [...prevState.searchResults, ...searchResults],
-          status: Status.RESOLVED,
         }));
-      })
-      .catch(err => this.setState({ status: Status.REJECTED }))
-      .finally(() => {
-        this.setState({ status: Status.RESOLVED });
         window.scrollTo({
           top: document.documentElement.scrollHeight,
           behavior: 'smooth',
         });
+      })
+      .catch(err => {
+        alert('Error! Try again!');
+        console.log(err);
       });
   };
 
@@ -79,50 +68,43 @@ export class ImageGallery extends Component {
   };
 
   render() {
-    const { searchResults, status, showModal, currentImg } = this.state;
-    if (status === 'idle') {
-      return <div></div>;
-    }
-    if (status === 'pending') {
-      return (
-        <Loader
-          type="Grid"
-          color="#00BFFF"
-          height={100}
-          width={100}
-          timeout={3000} //3 secs
-        />
-      );
-    }
-    if (status === 'resolved') {
-      return (
-        <>
-          <ul className="ImageGallery">
-            {searchResults.length > 0 &&
-              searchResults.map(item => {
-                return (
-                  <ImageGalleryItem
-                    key={item.id}
-                    obj={item}
-                    onClick={this.onClickImg}
-                  />
-                );
-              })}
-          </ul>
-          {searchResults.length > 0 && <Button onClick={this.handleClick} />}
-          {showModal && (
-            <Modal
-              onClose={this.toggleModal}
-              src={currentImg.largeImageURL}
-              alt={currentImg.tags}
-            ></Modal>
-          )}
-        </>
-      );
-    }
-    if (status === 'rejected') {
-      return alert('Error! Try again!');
-    }
+    const { searchResults, isLoading, showModal, currentImg } = this.state;
+    return (
+      <>
+        <ul className="ImageGallery">
+          {searchResults.length > 0 &&
+            searchResults.map(item => {
+              return (
+                <ImageGalleryItem
+                  key={item.id}
+                  obj={item}
+                  onClick={this.onClickImg}
+                />
+              );
+            })}
+        </ul>
+
+        {isLoading && (
+          <Loader
+            type="Grid"
+            color="#00BFFF"
+            height={100}
+            width={100}
+            timeout={3000} //3 secs
+          />
+        )}
+
+        {searchResults.length > 0 && <Button onClick={this.handleClick} />}
+
+        {showModal && (
+          <Modal
+            onClose={this.toggleModal}
+            src={currentImg.largeImageURL}
+            alt={currentImg.tags}
+          ></Modal>
+        )}
+      </>
+    );
   }
 }
 ImageGallery.propTypes = {
